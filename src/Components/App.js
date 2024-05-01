ea29b0f6a5a0e0b3f0af605be3bbc64c611ff98d
joseb <jbaldac02@gmail.com>
2024-05-01 00:45:55 -0500

    Added some styles to the Gallery.jsx

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+// Libraries
+import React from "react";
+// Components
+import Header from "./Header";
+import Gallery from "./Gallery";
+//Functions
+//Styles
+function App() {
+   return(
+       <div>
+       <Header></Header>
+       <Gallery></Gallery>
+      </div>
+   );
+}
+export default App
---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,20 @@
+function Gallery(){
+    return(
+        <div>
+      <div className="d-flex justify-content-center mb-4 font-italic pixelify-sans-title ">Gallery</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm"> Busca Minas </div>
+                <div className="col-sm"> Tetris </div>
+                <div className="col-sm"> Piedra Papel Tijeras </div>
+            </div>
+            <div className="row">
+                <div className="col-sm"> Wordle </div>
+                <div className="col-sm"> Sodoku </div>
+                <div className="col-sm"> Rompe Cabezas</div>
+            </div>
+        </div>
+      </div>
+    );
+}
+export default Gallery
