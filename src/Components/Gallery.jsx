78f6c644742dbd4f5acd4f91e4bd0d217c6771a8
joseb <jbaldac02@gmail.com>
2024-05-06 21:18:48 -0500

    Migrated to VsCode

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -9,21 +9,19 @@ import {Link} from "react-router-dom";
 
 function Gallery() {
     return (
-        <div>
+        <div className='d-flex flex-column'>
             <div className="d-flex justify-content-center mb-4 font-italic pixelify-sans-title ">Gallery</div>
-            <div className='d-flex justify-content-center mb-4 font-italic pixelify-sans-title'> Para probar un juego
-                Haga click en el Icono
-            </div>
+            <div className='d-flex justify-content-center mb-4 font-italic pixelify-sans-title '> Para probar un juego haga click en el Icono</div>
             <div className="container">
-                <div className="row">
-                    <Link to="/LandMineGame" className="col-sm" ><img src={LandMineIcon} alt="Imagen Icono Buscaminas" className="w-25 p-3"  /></Link>
-                    <Link to="/TetrisGame" className="col-sm w-25 p-3"><img src={TetrisIcon} alt="Imagen Icono Tetis"className="w-25 p-3" /></Link>
-                    <Link to="/PaperRockScissorsGame" className="col-sm w-25 p-3"><img src={PaperRockScissorsIcon} alt="Imagen Icono Piedra Papel  Tijeras" className="w-25 p-3" /></Link>
+                <div className="row justify-content-center">
+                    <Link to="/LandMineGame" className="col-sm" ><img src={LandMineIcon} alt="Imagen Icono Buscaminas" className="w-100 p-3"/></Link>
+                    <Link to="/TetrisGame" className="col-sm"><img src={TetrisIcon} alt="Imagen Icono Tetis"className="w-100 p-3" /></Link>
+                    <Link to="/PaperRockScissorsGame" className="col-sm"><img src={PaperRockScissorsIcon} alt="Imagen Icono Piedra Papel  Tijeras" className="w-100 p-3" /></Link>
                 </div>
-                <div className="row">
-                    <Link to="/WordleGame" className="col-sm w-25 p-3"><img src={WordleIcon} alt="Imagen Icono Wordle" className=" w-25 p-3" /></Link>
-                    <Link to="/SudokuGame" className="col-sm w-25 p-3"><img src={SudokuIcon} alt="Imagen Icono Sudoku" className="w-25 p-3" /></Link>
-                    <Link to="/SpaceInvadersGame" className="col-sm w-25 p-3"><img src={SpaceInvIcon} alt="Imagen Icono Space Invaders" className="w-25 p-3" /></Link>
+                <div className="row justify-content-center">
+                    <Link to="/WordleGame" className="col-sm"><img src={WordleIcon} alt="Imagen Icono Wordle" className=" w-100 p-3" /></Link>
+                    <Link to="/SudokuGame" className="col-sm"><img src={SudokuIcon} alt="Imagen Icono Sudoku" className="w-100 p-3" /></Link>
+                    <Link to="/SpaceInvadersGame" className="col-sm"><img src={SpaceInvIcon} alt="Imagen Icono Space Invaders" className="w-100 pl-5" /></Link>
                 </div>
             </div>
 
